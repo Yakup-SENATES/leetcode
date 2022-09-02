2ae35a09883e599e0c3e318dc82fd27deb937cae
Yakup SENATES <76456346+Yakup-SENATES@users.noreply.github.com>
2022-09-03 00:33:45 +0300

    Update DivideTwoIntegers.java

--- src/DivideTwoIntegers.java
+++ src/DivideTwoIntegers.java
@@ -22,6 +22,7 @@ public class DivideTwoIntegers {
         if (dividend == divisor) return 1;
         if (divisor == 1) return dividend;
         if (divisor == -1) return -dividend;
+        if (dividend == 0) return 0;
 
         int sign = (dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0) ? -1 : 1;
 
